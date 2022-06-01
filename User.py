cc467cb218e21f2cb4b0359b77c83ef76bfe5772
robertuptc <robertuptc@gmail.com>
2022-05-31 20:58:39 -0400

    finished exercise

--- User.py
+++ User.py
@@ -1,1 +1,17 @@
-# your User class goes here
+class User:
+    def __init__(self, name, age, phone, email):
+        self.name = name
+        self.age = age
+        self.phone = phone
+        self.email = email
+
+    def __str__(self):
+        return f"Hello, my name is {self.name} and I am {self.age} years old"
+
+    def contact_info(self):
+        return f"you can reach me at {self.phone}, and my email is {self.email}"
+
+charlie = User("Charlie", 18, "123-321-11", "robert@random.com")
+print(charlie)
+print(charlie.contact_info())
+
